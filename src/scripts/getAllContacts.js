3d1816ef923e88f1b6460a80093b9db8a34f17cd
Tetiana Savchuk <tanyainfo86@gmail.com>
2025-06-01 21:21:49 +0300

    2

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,14 +1,29 @@
-import fs from 'node:fs/promises';
-import { PATH_DB } from '../constants/contacts.js';
+// import fs from 'node:fs/promises';
+// import { PATH_DB } from '../constants/contacts.js';
 
+// export const getAllContacts = async () => {
+//     try {
+//         const data = await fs.readFile(PATH_DB, 'utf8');
+//         const dataObj = JSON.parse(data);
+//         return dataObj;
+//     } catch (error) {
+//         console.error(error);
+//     }
+// };
+
+// console.log(await getAllContacts());
 export const getAllContacts = async () => {
     try {
         const data = await fs.readFile(PATH_DB, 'utf8');
+        if (!data) return [];
         const dataObj = JSON.parse(data);
         return dataObj;
     } catch (error) {
+        if (error.code === 'ENOENT') {
+            console.warn(`File not found at ${PATH_DB}. Returning empty list.`);
+            return [];
+        }
         console.error(error);
+        throw error;
     }
 };
-
-console.log(await getAllContacts());
